5db27719d27e08a805adc50f223dc5a053a509cd
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2016-06-05 17:30:38 -0500

    Clase comparar prioridad

--- ProyectoIA/profundidad.h
+++ ProyectoIA/profundidad.h
@@ -2,6 +2,25 @@
 #define PROFUNDIDAD_H
 
 
+
+//Función de comparación
+//El operador le da mejor prioridad a los menores, si lo desea al reves cambie el operador por <=
+
+class CompararPrioridad
+{
+public:
+  CompararPrioridad()
+    {}
+  bool operator() (const Nodo& nodoA, const Nodo& nodoB) const
+  {
+     // cout << "nodoA.getCostoAcumulado() = " << nodoA.getCostoAcumulado() << endl;
+      Nodo  nodA = nodoA;
+      Nodo  nodB = nodoB;
+      return (nodA.getCostoAcumulado() >= nodB.getCostoAcumulado());
+  }
+};
+
+
 class Profundidad
 {
 
